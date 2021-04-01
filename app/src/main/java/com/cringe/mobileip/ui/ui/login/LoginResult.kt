e515e5340e03301fd2aea4507fd24c30ddd889a5
Tudor Craciun <tudor.craciun1@gmail.com>
2021-04-01 08:26:59 +0300

    Implemented login. Hardcoded starting an intent when pressing the login button.

---
+++ app/src/main/java/com/cringe/mobileip/ui/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.cringe.mobileip.ui.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
