e7cc5b69a8c25d961ba9b7d49a85984d0afaeef5
Tudor Craciun <tudor.craciun1@gmail.com>
2021-05-24 20:03:47 +0300

    Updated URLs.

--- app/src/main/java/com/cringe/mobileip/server/model/utils/Endpoints.kt
+++ app/src/main/java/com/cringe/mobileip/server/model/utils/Endpoints.kt
@@ -1,8 +1,8 @@
 package com.cringe.mobileip.server.model.utils
 
 object Endpoints {
-    var BASE_URL: String = "https://users-bd.herokuapp.com"
-    var loginURL: String = "$BASE_URL/api/v1/users/login"
-    var registerURL: String = "$BASE_URL/api/users/register"
+    var BASE_URL_AUTH: String = "https://users-bd.herokuapp.com/api"
+    var loginURL: String = "$BASE_URL_AUTH/users/login"
+    var registerURL: String = "$BASE_URL_AUTH/users/register"
     val requestNeedier: String = "https://all-db.herokuapp.com/api/v1/requestNeeder"
 }
